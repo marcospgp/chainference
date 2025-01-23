5c5ec3f072d4159f5dddb6f85bd00ac22bf84d93
Bernardo Rodrigues <bfar97@gmail.com>
2025-01-23 12:19:27 +0000

    feat: add frontend boilerplate

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+bun boilerplate react app 
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to React + Bun + TypeScript</h1>
+      <p>Start editing src/App.tsx to make changes</p>
+    </div>
+  );
+}
+
+export default App; 
