b0f8b3378da6b6f30398dcee06cc18646bd82177
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-01-25 14:05:36 +0000

    .

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider, Textarea, createTheme } from "@mantine/core";
 
 import "@mantine/core/styles.css";
 import "./index.css";
@@ -26,7 +26,14 @@ root.render(
           </div>
         </a>
       </div>
-      <div className="prompt-box">{/* <h1>Prompt the blockchain</h1> */}</div>
+      <div className="main">
+        <div className="prompt-box">
+          <h1>Prompt the blockchain</h1>
+          <div className="prompt-input">
+            <Textarea variant="unstyled" placeholder="Why is the sky blue?" />
+          </div>
+        </div>
+      </div>
     </MantineProvider>
   </StrictMode>
 );
