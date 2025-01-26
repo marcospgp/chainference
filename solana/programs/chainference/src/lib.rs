e116310e0716c6721c1dccad7e944180e150375b
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-01-26 23:23:52 +0000

    Update lib.rs

--- solana/programs/chainference/src/lib.rs
+++ solana/programs/chainference/src/lib.rs
@@ -10,21 +10,53 @@ declare_id!("FCLpnW5o1XceMGRgFma8WjHoekhKSmraZ21mfkMkzdNZ");
 pub mod chainference {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn announce_availability(
+        ctx: Context<AvailabilityAnnouncementInput>,
+        _space: u64,
+        models: Vec<ModelListing>,
+    ) -> Result<()> {
+        if models.len() > 256 {
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        let server_account = &mut ctx.accounts.server_account;
+
+        server_account.owner = ctx.accounts.server.key();
+        server_account.models = models;
+        server_account.last_heartbeat = Clock::get()?.unix_timestamp;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
-
 #[account]
 pub struct ServerAccount {
-    // Public key of the server that created the account
-    pub authority: Pubkey,
-    // Models and their prices per 1M tokens
-    pub models: Vec<(String, u64)>,
-    // Timestamp of last heartbeat
+    pub owner: Pubkey,
+    // Hugging face model IDs and corresponding price per 1M output tokens.
+    // Max len preallocates space for entries.
+    pub models: Vec<ModelListing>,
+    // Timestamp of last heartbeat.
     pub last_heartbeat: i64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct ModelListing {
+    pub id: String,
+    pub price: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(space: u64)]
+pub struct AvailabilityAnnouncementInput<'info> {
+    #[account(
+        init,
+        payer = server,
+        space = space as usize,
+        seeds = [b"server", server.key().as_ref()],
+        bump
+    )]
+    pub server_account: Account<'info, ServerAccount>,
+    #[account(mut)]
+    pub server: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
