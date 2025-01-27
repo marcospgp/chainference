ce0052e1d06ef08029a73ccc39bf8bf10fdc0ef9
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-01-27 11:02:36 +0000

    welcome solana (#3)
    * feat: css main client structure with flexbox
    
    * feat: more client design progress
    
    * .
    
    * Delete postcss.config.cjs
    
    * .
    
    * Update README.md
    
    * .
    
    * .
    
    * Update lib.rs
    
    * .
    
    ---------
    
    Co-authored-by: Bernardo Rodrigues <bfar97@gmail.com>

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,19 @@
-bun boilerplate react app 
+# Frontend
+
+The browser client which allows users to prompt models on the chain.
+
+[UI mockup in Figma](https://www.figma.com/design/ETdGpFo1859B8otLt4J8GH/chainference?node-id=8-2&t=12OrDJfxmJm9XZ8q-1)
+
+## Setup
+
+1. Install [bun](https://bun.sh/)
+1. `bun install`
+1. `bun run dev`
+
+`NODE_ENV` is set to `development` while developing as React's strict mode relies on it.
+
+## Styling
+
+We use [Mantine](https://mantine.dev/) as a UI library.
+
+We have a global CSS file that defines variables which components rely on for styling consistency.
---
+++ solana/programs/chainference/src/lib.rs
@@ -0,0 +1,62 @@
+// This line is required to avoid displaying some warnings.
+// See: https://github.com/coral-xyz/anchor/pull/3396
+#![allow(unexpected_cfgs)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("FCLpnW5o1XceMGRgFma8WjHoekhKSmraZ21mfkMkzdNZ");
+
+#[program]
+pub mod chainference {
+    use super::*;
+
+    pub fn list_server(
+        ctx: Context<ListServerInput>,
+        _space: u64,
+        models: Vec<ModelListing>,
+    ) -> Result<()> {
+        if models.len() > 256 {
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
+        let server_account = &mut ctx.accounts.server_listing_account;
+
+        server_account.owner = ctx.accounts.server.key();
+        server_account.models = models;
+        server_account.last_heartbeat = Clock::get()?.unix_timestamp;
+
+        Ok(())
+    }
+}
+
+#[account]
+pub struct ServerAccount {
+    pub owner: Pubkey,
+    // Hugging face model IDs and corresponding price per 1M output tokens.
+    // Max len preallocates space for entries.
+    pub models: Vec<ModelListing>,
+    // Timestamp of last heartbeat.
+    pub last_heartbeat: i64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+pub struct ModelListing {
+    pub id: String,
+    pub price: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(space: u64)]
+pub struct ListServerInput<'info> {
+    #[account(
+        init,
+        payer = server,
+        space = space as usize,
+        seeds = [b"server", server.key().as_ref()],
+        bump
+    )]
+    pub server_listing_account: Account<'info, ServerAccount>,
+    #[account(mut)]
+    pub server: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
