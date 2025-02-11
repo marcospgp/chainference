a61b6010a57d42835b1e2244f547e7ce137097fb
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-02-11 18:07:58 +0000

    logo v2

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,22 @@
-import './Navbar.css';
+import "./Navbar.css";
 // import { IoMenuOutline } from "react-icons/io5";
 // import { Button } from "@mantine/core";
 
-import david from '../../assets/david.svg';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { memo } from 'react';
+import david from "../../assets/david-v4.svg" with { type: "text" };
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { memo } from "react";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar-left'>
+    <div className="navbar">
         {/* <Button variant='default' onClick={onMenuClick}>
           <IoMenuOutline />
         </Button> */}
-        <a href='/'>
-          <div className='navbar-logo'>
-            <img
-              src={david}
-              alt='David'
-              width='50px'
-              height='50px'
-              className='navbar-logo-img'
-            />
-            <h1 className='navbar-logo-text'>Chainference</h1>
-          </div>
+        <a href="/" className="navbar-logo">
+          <div dangerouslySetInnerHTML={{ __html: david }}></div>
+          <h1>Chainference</h1>
         </a>
-      </div>
-      <div className='wallet'>
+      <div className="wallet">
         <WalletMultiButton />
       </div>
     </div>
