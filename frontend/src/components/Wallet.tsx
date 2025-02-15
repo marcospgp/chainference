b7dde7d3dd4f6ddd4457355cfcaa99bc82b4ea25
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2025-02-15 14:21:04 +0000

    .

--- frontend/src/components/Wallet.tsx
+++ frontend/src/components/Wallet.tsx
@@ -1,24 +1,91 @@
-import React, { type FC, useMemo } from 'react';
+import React, { type FC, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
+  AlphaWalletAdapter,
+  AvanaWalletAdapter,
+  BitpieWalletAdapter,
+  CloverWalletAdapter,
+  Coin98WalletAdapter,
+  CoinbaseWalletAdapter,
+  CoinhubWalletAdapter,
+  FractalWalletAdapter,
+  HuobiWalletAdapter,
+  HyperPayWalletAdapter,
+  KeystoneWalletAdapter,
+  KrystalWalletAdapter,
+  LedgerWalletAdapter,
+  MathWalletAdapter,
+  NekoWalletAdapter,
+  NightlyWalletAdapter,
+  NufiWalletAdapter,
+  OntoWalletAdapter,
+  ParticleAdapter,
   PhantomWalletAdapter,
+  SafePalWalletAdapter,
+  SaifuWalletAdapter,
+  SalmonWalletAdapter,
+  SkyWalletAdapter,
   SolflareWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+  SolongWalletAdapter,
+  SpotWalletAdapter,
+  TokenaryWalletAdapter,
+  TokenPocketWalletAdapter,
+  TorusWalletAdapter,
+  TrezorWalletAdapter,
+  TrustWalletAdapter,
+  UnsafeBurnerWalletAdapter,
+  XDEFIWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
-import '@solana/wallet-adapter-react-ui/styles.css';
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export const Wallet: FC<{ children: React.ReactNode }> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    () => [
+      new AlphaWalletAdapter(),
+      new AvanaWalletAdapter(),
+      new BitpieWalletAdapter(),
+      new CloverWalletAdapter(),
+      new Coin98WalletAdapter(),
+      new CoinbaseWalletAdapter(),
+      new CoinhubWalletAdapter(),
+      new FractalWalletAdapter(),
+      new HuobiWalletAdapter(),
+      new HyperPayWalletAdapter(),
+      new KeystoneWalletAdapter(),
+      new KrystalWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new MathWalletAdapter(),
+      new NekoWalletAdapter(),
+      new NightlyWalletAdapter(),
+      new NufiWalletAdapter(),
+      new OntoWalletAdapter(),
+      new ParticleAdapter(),
+      new PhantomWalletAdapter(),
+      new SafePalWalletAdapter(),
+      new SaifuWalletAdapter(),
+      new SalmonWalletAdapter(),
+      new SkyWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new SolongWalletAdapter(),
+      new SpotWalletAdapter(),
+      new TokenaryWalletAdapter(),
+      new TokenPocketWalletAdapter(),
+      new TorusWalletAdapter(),
+      new TrezorWalletAdapter(),
+      new TrustWalletAdapter(),
+      new UnsafeBurnerWalletAdapter(),
+      new XDEFIWalletAdapter(),
+    ],
     [network]
   );
 
